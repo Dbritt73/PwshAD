6e137fe383a0972793270c78ae79123517f6d923
Darrin Britton <brittod@wwu.edu>
2019-02-05 09:41:07 -0800

    Added more pipeline support Get-OldADcompuiters.ps1

--- Get-OldADComptuers.ps1
+++ Get-OldADComptuers.ps1
@@ -12,14 +12,20 @@ function Get-OldADComputers {
 
     Gets all computers in the OU Computers.Department that havent been logged into for 6 months or longer
 
+    .EXAMPLE
+    Get-OldADComputers -SearchBase 'Computers.Department','Computers.Department2' -MonthsOld '6'
+
+    Gets all computers in the OU Computers.Department and Computers.Depaertment2 that havent been logged into for 6 months or longer
+
     #>
     [CmdletBinding()]
     Param (
 
         [Parameter( Mandatory = $true,
                     HelpMessage = 'Add help message for user',
-                    ValueFromPipelineByPropertyName = $true)]
-        [String]$SearchBase,
+                    ValueFromPipelineByPropertyName = $true,
+                    ValueFromPipeline = $True)]
+        [String[]]$SearchBase,
 
         [string]$MonthsOld = 6,
 
@@ -35,60 +41,64 @@ function Get-OldADComputers {
 
             $LastLoginDeadline = (Get-Date).AddMonths(-$MonthsOld)
 
-            $ADC = @{
+            foreach ($orgUnit in $SearchBase) {
 
-                'SearchBase' = $SearchBase
+                $ADC = @{
 
-                'Filter'     = {(OperatingSystem -notlike '*Mac*') -And (OperatingSystem -notlike '*Ubuntu*') -And (LastLogonDate -lt $LastLoginDeadline)}
+                    'SearchBase' = $orgUnit
 
-                'Properties' = '*'
+                    'Filter'     = {(OperatingSystem -notlike '*Mac*') -And (OperatingSystem -notlike '*Ubuntu*') -And (LastLogonDate -lt $LastLoginDeadline)}
 
-                'ErrorAction' = 'Stop'
+                    'Properties' = '*'
 
-                'Credential' = $Credential
+                    'ErrorAction' = 'Stop'
 
-            }
-
-            if ($PSBoundParameters.ContainsKey(('Credential'))) {
-
-                $OldComputers = Get-ADComputer @ADC
-
-            } else {
-
-                $ADC.Remove('Credential')
-                $OldComputers = Get-ADComputer @ADC
-
-            }
-
-            Foreach ($computer in $OldComputers) {
-
-                $Bitlocker = Get-BitLockerInformation -ComputerName $Computer.Name
-
-                $Props = [ordered]@{
-
-                    'Name'            = $Computer.Name
-
-                    'Description'     = $computer.Description
-
-                    'LastLogon'       = $computer.LastLogonDate
-
-                    'GUID'            = $computer.ObjectGUID
-
-                    'OrgUnit'      = $computer.DistinguishedName.Split(',', 2)[1]
-
-                    'OperatingSystem' = $Computer.OperatingSystem
-
-                    'Enabled'         = $computer.Enabled
-
-                    'Bitlocker'       = if ($Bitlocker) {Write-Output -InputObject 'True'} Else {Write-Output -InputObject 'False'}
-
-                    'DateRecorded'    = (Get-Date)
+                    'Credential' = $Credential
 
                 }
 
-                $Object = New-Object -TypeName PSObject -Property $props
-                $object.PSObject.typenames.insert(0, 'Report.DatedComputers')
-                Write-Output -InputObject $Object
+                if ($PSBoundParameters.ContainsKey(('Credential'))) {
+
+                    $OldComputers = Get-ADComputer @ADC
+
+                } else {
+
+                    $ADC.Remove('Credential')
+                    $OldComputers = Get-ADComputer @ADC
+
+                }
+
+                Foreach ($computer in $OldComputers) {
+
+                    $Bitlocker = Get-BitLockerInformation -ComputerName $Computer.Name
+
+                    $Props = [ordered]@{
+
+                        'Name'            = $Computer.Name
+
+                        'Description'     = $computer.Description
+
+                        'LastLogon'       = $computer.LastLogonDate
+
+                        'GUID'            = $computer.ObjectGUID
+
+                        'OrgUnit'      = $computer.DistinguishedName.Split(',', 2)[1]
+
+                        'OperatingSystem' = $Computer.OperatingSystem
+
+                        'Enabled'         = $computer.Enabled
+
+                        'Bitlocker'       = if ($Bitlocker) {Write-Output -InputObject 'True'} Else {Write-Output -InputObject 'False'}
+
+                        'DateRecorded'    = (Get-Date)
+
+                    }
+
+                    $Object = New-Object -TypeName PSObject -Property $props
+                    $object.PSObject.typenames.insert(0, 'Report.DatedComputers')
+                    Write-Output -InputObject $Object
+
+                }
 
             }
 
