73f20408db0f98e639fbdb72c6ab8eb2f15d6759
Darrin Britton <brittod@wwu.edu>
2019-09-04 14:50:37 -0700

    Added AD ShadowGroups

---
+++ ADShadowGroups/ADShadowGroups/public/Add-ShadowGroupObjects.ps1
@@ -0,0 +1,103 @@
+Function Add-ADShadowGroupMembership {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Resolve-ShadowGroup" in 1-2 sentences.
+
+    .DESCRIPTION
+    Add a more complete description of what the function does.
+
+    .PARAMETER OU
+    Describe parameter -OU.
+
+    .PARAMETER ShadowGroup
+    Describe parameter -ShadowGroup.
+
+    .EXAMPLE
+    Resolve-ShadowGroup -OU Value -ShadowGroup Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Resolve-ShadowGroup
+    https://ravingroo.com/458/active-directory-shadow-group-automatically-add-ou-users-membership/
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
+
+    [CmdletBinding()]
+    Param (
+
+        [Parameter( Mandatory = $true,
+                    HelpMessage = 'Add help message for user')]
+        [String]$OU,
+
+        [Parameter( Mandatory = $true,
+                    HelpMessage = 'Add help message for user')]
+        [String]$ShadowGroup,
+
+        [Parameter()]
+        [pscredential]$Credential
+
+    )
+
+    Begin {}
+
+    Process {
+
+        Try {
+
+            $splat = @{
+
+                'SearchBase'  = $OU
+                'SearchScope' = 'OneLevel'
+                'LDAPFilter'  = "(!memberOf=$ShadowGroup)"
+                'ErrorAction' = 'Stop'
+
+            }
+
+            if ($PSBoundParameters.Contains('Credential')) {
+
+                $splat.Add('Credential', $Credential)
+
+            }
+
+            Get-ADObject @splat | ForEach-Object {
+
+                Add-ADGroupMember -Identity $ShadowGroup -Members $_ -Credential $Credential
+
+            }
+
+        } Catch {
+
+            # get error record
+            [Management.Automation.ErrorRecord]$e = $_
+
+            # retrieve information about runtime error
+            $info = [PSCustomObject]@{
+
+                Exception = $e.Exception.Message
+                Reason    = $e.CategoryInfo.Reason
+                Target    = $e.CategoryInfo.TargetName
+                Script    = $e.InvocationInfo.ScriptName
+                Line      = $e.InvocationInfo.ScriptLineNumber
+                Column    = $e.InvocationInfo.OffsetInLine
+
+            }
+
+            # output information. Post-process collected info, and log info (optional)
+            Write-Output -InputObject $info
+
+        }
+
+    }
+
+    End {}
+
+}
---
+++ ADShadowGroups/ADShadowGroups/public/Remove-ShadowGroupObjects.ps1
@@ -0,0 +1,94 @@
+Function Remove-ADShadowGroupMembership {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Resolve-ShadowGroup" in 1-2 sentences.
+
+    .DESCRIPTION
+    Add a more complete description of what the function does.
+
+    .PARAMETER OU
+    Describe parameter -OU.
+
+    .PARAMETER ShadowGroup
+    Describe parameter -ShadowGroup.
+
+    .EXAMPLE
+    Resolve-ShadowGroup -OU Value -ShadowGroup Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Resolve-ShadowGroup
+    https://ravingroo.com/458/active-directory-shadow-group-automatically-add-ou-users-membership/
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
+
+    [CmdletBinding()]
+    Param (
+
+        [Parameter( Mandatory = $true,
+                    HelpMessage = 'Add help message for user')]
+        [String]$OU,
+
+        [Parameter( Mandatory = $true,
+                    HelpMessage = 'Add help message for user')]
+        [String]$ShadowGroup
+
+    )
+
+    Begin {}
+
+    Process {
+
+        Try {
+
+            $Splat = @{
+
+                'Identity'    = $ShadowGroup
+                'ErrorAction' = 'Stop'
+
+            }
+
+            $Member = Get-ADGroupMember @Splat | Where-Object {$_.distinguishedName -NotMatch $OU}
+
+            $Member | ForEach-Object {
+
+                Remove-ADPrincipalGroupMembership -Identity $_ -MemberOf $ShadowGroup -Confirm:$false
+
+            }
+
+        } Catch {
+
+            # get error record
+            [Management.Automation.ErrorRecord]$e = $_
+
+            # retrieve information about runtime error
+            $info = [PSCustomObject]@{
+
+                Exception = $e.Exception.Message
+                Reason    = $e.CategoryInfo.Reason
+                Target    = $e.CategoryInfo.TargetName
+                Script    = $e.InvocationInfo.ScriptName
+                Line      = $e.InvocationInfo.ScriptLineNumber
+                Column    = $e.InvocationInfo.OffsetInLine
+
+            }
+
+            # output information. Post-process collected info, and log info (optional)
+            Write-Output -InputObject $info
+
+        }
+
+    }
+
+    End {}
+
+}
